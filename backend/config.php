22a92ebaeb4b0b02e3524b455056345be2a8ba80
Wojciech <kowalczyk.wojtek168@gmail.com>
2022-12-06 20:08:30 +0100

    main site with backend almost done

---
+++ backend/config.php
@@ -0,0 +1,10 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $db = 'ecommerce';
+
+    $conn = new mysqli($host, $user, $password, $db);
+
+    // if($conn) echo 'dziala';
+    // else echo 'nie dziala';
